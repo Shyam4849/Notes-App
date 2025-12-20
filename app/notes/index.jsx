0a6e3e95f7df3186a23d008506e13de3dfb63339
Shyam4849 <piyush.soni8877@gmail.com>
2025-12-20 11:57:05 +0530

    .

--- app/notes/index.jsx
+++ app/notes/index.jsx
@@ -1,25 +1,48 @@
 import AddNoteModal from "@/components/AddNoteModal";
 import NoteList from "@/components/NoteList";
-import { useState } from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import noteService from "@/services/noteService";
+import { useEffect, useState } from "react";
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const NoteScreen = () => {
-    const [notes, setNotes] = useState([
-        { id: '1', text: 'Note One' },
-        { id: '2', text: 'Note Two' },
-        { id: '3', text: 'Note Three' },
-    ]);
+    const [notes, setNotes] = useState([]);
     const [modalVisible, setModalVisible] = useState(false);
     const [newNote, setNewNote] = useState('');
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetchNotes();
+    }, []);
+
+    const fetchNotes = async () => {
+        setLoading(true);
+        const response = await noteService.getNotes();
+
+        if (response.error) {
+            setError(response.error);
+            Alert.alert('Error', response.error);
+        }
+        else {
+            setNotes(response.data);
+            setError(null);
+        }
+
+        setLoading(false);
+    };
 
     // Add New Note
-    const addNote = () => {
+    const addNote = async () => {
         if (newNote.trim() === '') return;
 
-        setNotes((prevNotes) => [
-            ...prevNotes,
-            { id: Date.now().toString(), text: newNote }
-        ]);
+        const response = await noteService.addNote(newNote);
+
+        if (response.error) {
+            Alert.alert('Error', response.error);
+        }
+        else {
+            setNotes([...notes, response.data]);
+        }
 
         setNewNote('');
         setModalVisible(false);
@@ -27,7 +50,7 @@ const NoteScreen = () => {
 
     return (
         <View style={styles.container}>
-            <NoteList notes={notes}/>
+            <NoteList notes={notes} />
 
             <TouchableOpacity style={styles.addButton} onPress={() => setModalVisible(true)}>
                 <Text style={styles.addButtonText}>+ Add Note</Text>
@@ -41,7 +64,7 @@ const NoteScreen = () => {
                 setNewNote={setNewNote}
                 addNote={addNote}
             />
-            
+
         </View >);
 }
 
--- services/databaseService.js
+++ services/databaseService.js
@@ -6,11 +6,20 @@ const databaseService = {
         try {
             const response = await database.listDocuments(dbId, colId);
             return response.documents || [];
-            } catch (error) {
-                console.error('Error fetching documents:', error.message);
-                return {error: error.message}
+        } catch (error) {
+            console.error('Error fetching documents:', error.message);
+            return { error: error.message }
         }
-    }
+    },
+    // Create Documents
+    async createDocument(dbId, colId, data, id = null) {
+        try {
+            return await database.createDocument(dbId, colId, id || undefined, data);
+        } catch (error) {
+            console.error('Error creating document', error.message);
+            return { error: error.message, };
+        }
+    },
 };
 
 export default databaseService;
